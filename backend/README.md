772eaf84f6288f30dff2b38d8c0c0ff447b04adb
h3ssto <me@h3ssto.net>
2023-10-16 02:57:41 +0200

    Initial commit of simple analysis backend

---
+++ backend/README.md
@@ -0,0 +1,43 @@
+# comvy: Built-In Backend
+
+Simple analysis backend built with [Flask](https://flask.palletsprojects.com) and [Flask-Caching](https://flask-caching.readthedocs.io).
+
+## Routes
+
+* `/register_formula`: Register a formula in DIMACS format 
+    * Returns `417` when the file is not valid DIMACS
+    * Returns `422` if no file is supplied
+    * Returns `200` and key `ident` for further actions on the formula
+
+> Example: `curl -i -X POST -F file=@<file> localhost:5000/register_formula`
+> HTTP/1.1 200 OK
+> tmpa6nz31px
+
+* `/view_formula/<ident>`: View the DIMACS of a registered formula
+    * Returns `404` if `ident` is invalid
+    * Returns `200` and raw content of the DIMACS file
+
+* `/analysis/sat/<ident> `: Verify a (partial) configuration as JSON
+    * Returns `404` if `ident` is invalid
+    * Returns `200` and a JSON
+
+> Example: `curl -X POST localhost:5000/verify/<ident> -H 'Content-Type: application/json' -d '{"config":[1, 2, 3]}'`
+> {"valid": True, "solution": [1,2,3,...]} # if [1,2,3] is a valid partial configuration
+> {"valid": False, "refutation": [1,2,3]} # if [1,2,3] is an invalid partial configuration
+
+* `/analysis/dp/<ident> `: Compute decision propagation of a (partial) configuration supplied as JSON
+    * Returns `404` if `ident` is invalid
+    * Returns `200` and a JSON containing sets of free, implicit selected, and implicit deselected variables
+
+> Example: `curl -X POST localhost:5000/verify/<ident> -H 'Content-Type: application/json' -d '{"config":[1, 2, 3]}'`
+> {"free": [..], "implicit_selected": [..], "implicit_deselected": [..]}
+
+* `/analysis/deadcore/<ident> `: Compute dead and core variables of the formula
+    * Returns `404` if `ident` is invalid
+    * Returns `200` and a JSON containing sets of dead and core variables
+
+> Example: `curl -X POST localhost:5000/analysis/deadcore/<ident>`
+> {"deads": [..], "cores": [..]}
+
+
+
---
+++ backend/app.py
@@ -0,0 +1,166 @@
+import os
+from os import path
+from flask import Flask, flash, request, Response, jsonify
+from flask_caching import Cache
+
+from pysat.formula import CNF
+from pysat.solvers import Solver
+
+from tempfile import NamedTemporaryFile
+
+from copy import copy
+
+UPLOAD_FOLDER = '/tmp/'
+
+cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = os.urandom(32)
+
+cache.init_app(app)
+
+
+solvers = dict()
+
+
+@app.route('/register_formula', methods = ["POST"])
+def register_file():
+    if "file" not in request.files:
+        flash("no file supplied")
+        return Response("no file supplied", status = 422)
+
+    file = request.files["file"]
+
+    if not file or file.filename == "":
+        flash("no file supplied")
+        return Response("no file supplied", status = 422)
+
+    persname = NamedTemporaryFile(dir = UPLOAD_FOLDER).name
+    file.save(persname)
+
+    ident = path.basename(persname)
+
+    try:
+        formula = CNF(from_file = persname)
+        solvers[ident] = Solver(bootstrap_with = formula)
+
+        cache.set(f"{ident}", formula)
+
+        return Response(ident, status = 200)
+    except ValueError:
+        return Response("invalid CNF", status = 417)
+
+
+@app.route('/view_formula/<ident>', methods = ["GET", "POST"])
+def view_formula(ident):
+
+    if (formula := cache.get(ident)) is None:
+        return Response(f"key {ident} unknown", status = 404)
+
+    with open(path.join(UPLOAD_FOLDER, ident)) as file:
+        data = file.read()
+
+    return Response(data, status = 200)
+
+
+@app.route('/analysis/sat/<ident>', methods = ["POST"])
+def verify_config(ident):
+    data = request.get_json()
+
+    if (formula := cache.get(ident)) is None:
+        print(ident, "unknown")
+        return Response(f"key {ident} unknown", status = 404)
+
+    config = data.get("config", None)
+
+    if config is None or type(config) != list:
+        return Response(f"{config} is not a valid configuration ([int] required)")
+
+    if (solver := solvers.get(ident)) is None:
+        solver = Solver(bootstrap_with = formula)
+
+    if solver.solve(config):
+        data = dict(valid = True, solution = solver.get_model())
+    else:        
+        data = dict(valid = False, refutation = solver.get_core())
+
+    return jsonify(data), 200
+
+
+def temp_config(config, x):
+    config = copy(config)
+    config.append(x)
+
+    return config
+
+
+@app.route('/analysis/dp/<ident>', methods = ["POST"])
+def dp(ident):
+    data = request.get_json()
+    config = data.get("config", None)
+
+    if config is None or type(config) != list:
+        return Response(f"{config} is not a valid configuration ([int] required)")
+
+    if (formula := cache.get(ident)) is None:
+        print(ident, "unknown")
+        return Response(f"key {ident} unknown", status = 404)
+
+    if (solver := solvers.get(ident)) is None:
+        solver = Solver(bootstrap_with = formula)
+
+    simp = {x for x in range(1, formula.nv + 1) if not solver.solve(temp_config(config, -x))}.difference(config)
+    dimp = {x for x in range(1, formula.nv + 1) if not solver.solve(temp_config(config, x))}.difference(config)
+
+    free = set(range(1, formula.nv + 1)).difference(simp).difference(dimp)
+    data = dict(implicit_selected = sorted(simp), implicit_deselected = sorted(dimp), free = sorted(free))
+
+    return jsonify(data), 200
+
+
+@app.route('/analysis/deadcore/<ident>', methods = ["POST"])
+@cache.cached()
+def deadcore(ident):
+    if (formula := cache.get(ident)) is None:
+        print(ident, "unknown")
+        return Response(f"key {ident} unknown", status = 404)
+
+    if (solver := solvers.get(ident)) is None:
+        solver = Solver(bootstrap_with = formula)
+
+    found = set()
+
+    dead = set()
+    core = set()
+
+    
+    for x in range(1, formula.nv + 1):
+        x_found = x in found
+        nx_found = -x in found
+
+        if x_found and nx_found:
+            continue
+
+        if not x_found:
+            if solver.solve([x]):
+                for y in solver.get_model():
+                    found.add(y)
+            else:
+                solver.add_clause([-x])
+                dead.add(x)
+
+        if not nx_found:
+            if solver.solve([-x]):
+                for y in solver.get_model():
+                    found.add(y)
+            else:
+                solver.add_clause([x])
+                core.add(x)
+
+    data = {
+        "cores": sorted(core),
+        "deads": sorted(dead)
+    }
+
+    return jsonify(data), 200
